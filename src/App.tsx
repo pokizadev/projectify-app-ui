adf122c5bc82fecad91c4600951ba84d3a8a0f42
PokizaSdv <pokizasdv@gmail.com>
2023-12-25 00:34:10 -0500

    Convert to TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "./design-system/Typography";
+
+function App() {
+    return (
+        <>
+            <Typography variant="h5">Hello</Typography>
+        </>
+    );
+}
+
+export { App };
