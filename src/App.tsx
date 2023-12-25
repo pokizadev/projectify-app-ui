a3cf84e9b8fdf85c27babe9c209f3b989da7cf4e
PokizaSdv <pokizasdv@gmail.com>
2023-12-25 01:41:06 -0500

    Create Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,111 @@
-import { Typography } from "./design-system/Typography";
+import { Button, Typography } from "./design-system";
 
 function App() {
     return (
-        <>
-            <Typography variant="h5">Hello</Typography>
-        </>
+      <div style={{ padding: "100px" }}>
+      <Typography variant="h5">Hello</Typography>
+      <div className="buttons">
+          <Button disabled>Button</Button>
+      </div>
+      <div className="buttons">
+          <Button color="primary">Button</Button>
+          <Button color="primary" disabled>
+              Button
+          </Button>
+          <Button color="primary" variant="outlined">
+              Button
+          </Button>
+          <Button color="primary" variant="outlined" disabled>
+              Button
+          </Button>
+          <Button color="primary" variant="text">
+              Button
+          </Button>
+          <Button color="primary" variant="text" disabled>
+              Button
+          </Button>
+      </div>
+      <div className="buttons">
+          <Button color="primary" shape="rounded">
+              Button
+          </Button>
+          <Button color="primary" disabled shape="rounded">
+              Button
+          </Button>
+          <Button color="primary" variant="outlined" shape="rounded">
+              Button
+          </Button>
+          <Button
+              color="primary"
+              variant="outlined"
+              disabled
+              shape="rounded"
+          >
+              Button
+          </Button>
+          <Button color="primary" variant="text" shape="rounded">
+              Button
+          </Button>
+          <Button color="primary" variant="text" disabled shape="rounded">
+              Button
+          </Button>
+      </div>
+      <div className="buttons">
+          <Button color="secondary" shape="rounded">
+              Button
+          </Button>
+          <Button color="secondary" disabled shape="rounded">
+              Button
+          </Button>
+          <Button color="secondary" variant="outlined" shape="rounded">
+              Button
+          </Button>
+          <Button
+              color="secondary"
+              variant="outlined"
+              disabled
+              shape="rounded"
+          >
+              Button
+          </Button>
+          <Button color="secondary" variant="text" shape="rounded">
+              Button
+          </Button>
+          <Button
+              color="secondary"
+              variant="text"
+              disabled
+              shape="rounded"
+          >
+              Button
+          </Button>
+      </div>
+      <div className="buttons">
+          <Button color="danger" shape="rounded">
+              Button
+          </Button>
+          <Button color="danger" disabled shape="rounded">
+              Button
+          </Button>
+          <Button color="danger" variant="outlined" shape="rounded">
+              Button
+          </Button>
+          <Button
+              color="danger"
+              variant="outlined"
+              disabled
+              shape="rounded"
+          >
+              Button
+          </Button>
+          <Button color="danger" variant="text" shape="rounded">
+              Button
+          </Button>
+          <Button color="danger" variant="text" disabled shape="rounded">
+              Button
+          </Button>
+      </div>
+  </div>
     );
 }
 
---
+++ src/design-system/Button/Button.tsx
@@ -0,0 +1,70 @@
+import React, { FC } from "react";
+import "./Button.css";
+
+type ButtonSize = "sm" | "md" | "lg";
+type ButtonShape = "rounded" | "circle";
+type ButtonColor = "primary" | "secondary" | "danger";
+type ButtonVariant = "contained" | "outlined" | "text";
+
+type ButtonProps = {
+    size?: ButtonSize;
+    shape?: ButtonShape;
+    color?: ButtonColor;
+    variant?: ButtonVariant;
+    disabled?: boolean;
+    className?: string;
+    children: React.ReactNode;
+};
+
+const sizeClassNames = {
+    sm: "btn-small",
+    md: "btn-medium",
+    lg: "btn-large",
+};
+
+const shapeClassNames = {
+    rounded: "btn-rounded",
+    circle: "btn-circle",
+};
+
+const colorClassNames = {
+    primary: "btn-primary",
+    secondary: "btn-secondary",
+    danger: "btn-danger",
+};
+
+const variantClassNames = {
+    contained: "btn-contained",
+    outlined: "btn-outlined",
+    text: "btn-text",
+};
+
+const Button: FC<ButtonProps> = (props) => {
+    const { size, shape, color, variant, disabled, className, children } =
+        props;
+
+    const sizeClassName = size !== undefined ? sizeClassNames[size] : "";
+
+    const shapeClassName = shape !== undefined ? shapeClassNames[shape] : "";
+
+    const colorClassName = color !== undefined ? colorClassNames[color] : "";
+
+    const variantClassName =
+        variant !== undefined ? variantClassNames[variant] : "";
+
+    const finalClassNames = `btn ${colorClassName} ${sizeClassName} ${shapeClassName} ${variantClassName} ${
+        className || ""
+    }`;
+
+    return (
+        <button
+            className={finalClassNames}
+            disabled={disabled}
+            onClick={() => alert("Hello")}
+        >
+            {children}
+        </button>
+    );
+};
+
+export { Button };
