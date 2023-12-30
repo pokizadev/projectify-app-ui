788ec32c0c3b123834360c9c9711400971e663f6
PokizaSdv <pokizasdv@gmail.com>
2023-12-30 02:24:03 -0500

    Create Reusable wrapper Component for Auth pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useState, useId } from "react";
 
 import { Button, Typography, Input, Label, Avatar, Modal } from "./design-system";
 
+import { Signup } from "./pages/admin-auth/Signup";
+
 const App = () => {
     const [show, setShow] = useState(false);
     const emailId = useId();
 
     return (
         <div style={{ padding: "100px" }}>
-            
+            <Signup />;
         </div>
     );
 };
