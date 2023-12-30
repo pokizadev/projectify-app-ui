61731290af04075cb5a93d7368ea705bbbff6a46
PokizaSdv <pokizasdv@gmail.com>
2023-12-30 01:50:22 -0500

    Create Modal Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useId } from "react";
 
-import { Button, Typography, Input, Label } from "./design-system";
+import { Button, Typography, Input, Label, Avatar, Modal } from "./design-system";
 
 const App = () => {
     const [show, setShow] = useState(false);
@@ -8,59 +8,7 @@ const App = () => {
 
     return (
         <div style={{ padding: "100px" }}>
-            <Typography variant="h5">Hello</Typography>
-            <div className="buttons">
-                <Button color="primary">Button</Button>
-                <Button color="primary" disabled>
-                    Button
-                </Button>
-                <Button color="primary" variant="outlined">
-                    Button
-                </Button>
-                <Button color="primary" variant="outlined" disabled>
-                    Button
-                </Button>
-                <Button color="primary" variant="text">
-                    Button
-                </Button>
-                <Button color="primary" variant="text" disabled>
-                    Button
-                </Button>
-            </div>
-
-            <form onSubmit={() => alert("submitted")}>
-                <Input
-                    type="email"
-                    placeholder="Email"
-                    size="md"
-                    shape="rounded"
-                    labelText="Email"
-                />
-                <Input
-                    placeholder="First Name"
-                    size="md"
-                    shape="rounded"
-                    labelText="First Name"
-                />
-                <Input
-                    placeholder="Last Name"
-                    size="md"
-                    shape="rounded"
-                    labelText="Last Name"
-                />
-                <Input
-                    placeholder="About You"
-                    size="lg"
-                    type="textarea"
-                    shape="rounded"
-                    labelText="Tell us about yourself"
-                    hintMessage="This is for your password"
-                />
-
-                <Button>Submit</Button>
-            </form>
-
-            {show && <p>Hello</p>}
+            
         </div>
     );
 };
--- src/design-system/index.ts
+++ src/design-system/index.ts
@@ -2,3 +2,5 @@ export * from "./Typography";
 export * from "./Button";
 export * from "./Label";
 export * from "./Input"
+export * from "./Avatar"
+export * from "./Modal"
