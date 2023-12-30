1202d8033eb9352257dfdfacf6b54ed839b46010
PokizaSdv <pokizasdv@gmail.com>
2023-12-29 23:53:42 -0500

    Create Avatar Component

--- src/design-system/utils/index.ts
+++ src/design-system/utils/index.ts
@@ -1,3 +1,9 @@
 export const trimWhiteSpaces = (str: string) => {
     return str.split(" ").filter((item) => item !== "").join(" ")
 }
+
+export const getNameInitials = (name: string): string => {
+    const words = name.split(" ");
+    const initials = words.map((word) => word.charAt(0).toUpperCase()).join("");
+    return initials;
+};
