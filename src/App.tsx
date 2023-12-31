1e32f500befa70f506ab2e80a539cafe45a61eb3
PokizaSdv <pokizasdv@gmail.com>
2023-12-30 23:17:40 -0500

    Create signup page for admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import { useState, useId } from "react";
+// import { useState, useId } from "react";
 
-import { Button, Typography, Input, Label, Avatar, Modal } from "./design-system";
+// import { Button, Typography, Input, Label, Avatar, Modal } from "./design-system";
 
-import { Signup } from "./pages/admin-auth/Signup";
+import { Signup } from "./pages";
 
 const App = () => {
-    const [show, setShow] = useState(false);
-    const emailId = useId();
-
     return (
-        <div style={{ padding: "100px" }}>
+        <div>
             <Signup />;
         </div>
     );
 };
-<button type="submit"></button>;
 
 export { App };
