34f295f55fc58c480a4e69f09ded6840cf5916c2
PokizaSdv <pokizasdv@gmail.com>
2024-01-09 23:00:28 -0500

    Create App Layout and restructure files

--- src/pages/components/index.ts
+++ src/pages/components/index.ts
@@ -1,4 +1,4 @@
-export * from "./auth-wrapper/AuthWrapper";
-export * from "./password-wrapper/PasswordWrapper"
-export * from "./main-layout/MainLayout"
-export * from "./defaul-page-wrapper/DefaultPageWrapper"
+export * from "./AuthWrapper";
+export * from "./PasswordWrapper";
+export * from "./AppLayout"
+
