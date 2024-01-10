67a9676fadeda0b67490b80232133386a2a33df2
PokizaSdv <pokizasdv@gmail.com>
2024-01-09 23:53:36 -0500

    Refactor SideBar

--- src/pages/admin-platform/Platform.tsx
+++ src/pages/admin-platform/Platform.tsx
@@ -1,15 +1,56 @@
-import { Link, Outlet } from "react-router-dom";
-import { SideBar } from "../../design-system";
-import { AppContent, AppLayout } from "../components/AppLayout";
+import { Outlet } from "react-router-dom";
+import { SideBar, SideBarLinks } from "../../design-system";
+import { AppContent, AppLayout, User } from "../components/";
+
+const links = [
+    {
+        title: "Menu",
+        links: [
+            {
+                linkText: "Project",
+                linkTo: "projects",
+                iconName: "projects"
+            },
+            {
+                linkText: "Stories",
+                linkTo: "stories",
+                iconName: "stories"
+            },
+            {
+                linkText: "Personal Tasks",
+                linkTo: "personal-tasks",
+                iconName: "tasks"
+            },
+            {
+                linkText: "Team Members",
+                linkTo: "team-members",
+                iconName: "members"
+            }
+        ]
+    },
+    {
+        title: "Settings",
+        links: [
+            {
+                linkText: "Settings",
+                linkTo: "settings",
+                iconName: "settings"
+            },
+            {
+                linkText: "Support",
+                linkTo: "support",
+                iconName: "support"
+            }
+        ]
+    }
+]
 
 const Platform = () => {
     return (
         <AppLayout>
             <SideBar>
-                <Link to="projects" >Project</Link>
-                <Link to="stories" >Stories</Link>
-                <Link to="personal-tasks">Personal Tasks</Link>
-                <Link to="team-members" >Team Members</Link>
+                <User/>
+                <SideBarLinks links={links}/>
             </SideBar>
             <AppContent>
                 <Outlet />
