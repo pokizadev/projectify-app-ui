9190e4c23b2afbebeaa8de785e4581c9723ae7ff
PokizaSdv <pokizasdv@gmail.com>
2024-01-12 03:13:23 -0500

    Rename files

--- src/design-system/utils/index.ts
+++ src/design-system/utils/index.ts
@@ -1,9 +1,1 @@
-export const trimWhiteSpaces = (str: string) => {
-    return str.split(" ").filter((item) => item !== "").join(" ")
-}
-
-export const getNameInitials = (name: string): string => {
-    const words = name.split(" ");
-    const initials = words.map((word) => word.charAt(0).toUpperCase()).join("");
-    return initials;
-};
+export * from "./utils"
