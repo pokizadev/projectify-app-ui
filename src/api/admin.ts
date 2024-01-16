513557d1f81522232b802e1eb95c6afe31102ab7
PokizaSdv <pokizasdv@gmail.com>
2024-01-15 23:33:29 -0500

    Create api calls for Admin Signup

---
+++ src/api/admin.ts
@@ -0,0 +1,49 @@
+type SignUpInput = {
+    firstName: string;
+    lastName: string;
+    preferredName?: string;
+    email: string;
+    password: string;
+    company?: {
+        name: string;
+        position: string;
+    };
+};
+
+type SignInInput = {
+    email: string;
+    password: string;
+};
+
+class Admin {
+    url: string;
+    constructor() {
+        this.url = `${
+            process.env.NODE_ENV === "development"
+                ? process.env.REACT_APP_PROJECTIFY_API_URL_LOCAL
+                : process.env.REACT_APP_PROJECTIFY_API_URL
+        }/admins`;
+    }
+    async signUp(input: SignUpInput) {
+        try {
+            const response = await fetch(`${this.url}/sign-up`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(input),
+            });
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+            return response.json();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    
+}
+
+export const admin = new Admin();
