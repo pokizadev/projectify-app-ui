3697b302ec1a5e049a338c048351cf4fab7babb4
PokizaSdv <pokizasdv@gmail.com>
2024-01-21 18:12:06 -0500

    Create Team member Platform

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,12 @@ export * from "./admin-auth/admin-sign-up/AdminSignup";
 export * from "./admin-auth/admin-login/AdminLogin"
 export * from "./admin-auth/admin-forgot-password/AdminForgotPassword"
 export * from "./admin-auth/admin-reset-password/AdminResetPassword"
+export * from "./admin-platform/AdminPlatform"
+export * from "./admin-platform/projects/Projects"
+
+
 export * from "./team-member-auth/team-member-create-password/TeamMemberCreatePassword"
 export * from "./team-member-auth/team-member-login/TeamMemberLogin"
 export * from "./team-member-auth/team-member-forgot-password/TeamMemberForgotPassword"
 export * from "./team-member-auth/team-member-reset-password/TeamMemberResetPassword";
-export * from "./admin-platform/Platform"
-export * from "./admin-platform/projects/Projects"
+export * from "./team-member-platform/TeamMemberPlatform"
