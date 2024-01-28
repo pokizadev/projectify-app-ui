4269f2457e1a50245e05db365362b5033e5de806
PokizaSdv <pokizasdv@gmail.com>
2024-01-27 23:53:12 -0500

    Improve Authorized Route

--- src/router/Private.tsx
+++ src/router/Private.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useLocalStorage, useStore } from "../hooks";
 import { admin } from "../api";
 import { UserRole } from "../types/types";
@@ -9,46 +9,62 @@ import toast from "react-hot-toast";
 type ProtectedRouteProps = {
     component: React.ReactElement;
     userType: UserRole;
-    to: string;
 };
 
 const Private: React.FC<ProtectedRouteProps> = ({
     component,
     userType,
-    to
 }) => {
     const { getItem, setItem } = useLocalStorage();
-    const { state, dispatch } = useStore();
-    
+    const { dispatch } = useStore();
+    const navigate = useNavigate()
+
     let isAuthTokenExists = getItem("authToken");
 
     useEffect(() => {
-        if (userType === "admin") {
-            admin
-                .getMe()
-                .then((data): void => {
-                    const action: InitUserAction = {
-                        type: Actions.INIT_USER,
-                        payload: data.data
-                    };
-                    dispatch(action);
-                    setItem("userRole", data.data.role)
-                })
-                .catch((error: Error) => {
-                    toast.error(error.message);
-                });
-        } else if(userType === "teamMember") {
-            
+        if(isAuthTokenExists) {
+            if (userType === "admin") {
+                admin
+                    .getMe()
+                    .then((data): void => {
+                        const action: InitUserAction = {
+                            type: Actions.INIT_USER,
+                            payload: data.data
+                        };
+                        dispatch(action);
+                        setItem("userRole", data.data.role);
+                    })
+                    .catch((error: Error) => {
+                        toast.error(error.message);
+                        navigate("../")
+
+                    });
+            } else if (userType === "teamMember") {
+            }
         }
-    }, [dispatch]);
+       
+    }, [userType]);
 
-    const isAuthorized = getItem("userRole") === userType;
+    const userRole = getItem("userRole");
+    const isAuthorized = userType === userRole;
 
-    if (isAuthTokenExists && isAuthorized) {
+    if (!isAuthTokenExists) {
+        const navigateTo =
+            userType === "admin"
+                ? "../admin/login"
+                : "../team-member/login";
+        return <Navigate to={navigateTo} />;
+    } else if (isAuthorized) {
         return component;
-    } else {
-        return <Navigate to={to} />;
+    } else if (!isAuthorized) {
+        const navigateTo =
+            userType === "admin"
+                ? "../admin/platform"
+                : "../team-member/platform";
+        return <Navigate to={navigateTo} />;
     }
+        return <Navigate to="../" />;
+    
 };
 
 export { Private };
--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -18,45 +18,46 @@ import {
     TeamMemberPlatform
 } from "../pages";
 import { Private } from "./Private";
+import { Auth } from "./Auth";
 
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <Route path="/" element={<App />} />
-            <Route path="/admin/signup" element={<AdminSignup />} />
-            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin/sign-up" element={<Auth component={<AdminSignup/>} userType="admin"/>} />
+            <Route path="/admin/login"  element={<Auth component={<AdminLogin/>} userType="admin"/>}/>
             <Route
                 path="/admin/forgot-password"
-                element={<AdminForgotPassword />}
+                element={<Auth component={<AdminForgotPassword/>} userType="admin"/>}
             />
             <Route
                 path="/admin/reset-password"
-                element={<AdminResetPassword />}
+                element={<Auth component={<AdminResetPassword/>} userType="admin"/>}
             />
 
             <Route
                 path="/team-member/create-password"
-                element={<TeamMemberCreatePassword />}
+                element={<Auth component={<TeamMemberCreatePassword/>} userType="teamMember"/>}
             />
-            <Route path="/team-member/login" element={<TeamMemberLogin />} />
+            <Route path="/team-member/login" element={<Auth component={<TeamMemberLogin/>} userType="teamMember"/>} />
             <Route
                 path="/team-member/forgot-password"
-                element={<TeamMemberForgotPassword />}
+                element={<Auth component={<TeamMemberForgotPassword/>} userType="teamMember"/>}
             />
             <Route
                 path="/team-member/reset-password"
-                element={<TeamMemberResetPassword />}
+                element={<Auth component={<TeamMemberResetPassword/>} userType="teamMember"/>}
             />
 
-            <Route path="/admin/platform" element={<Private component={<AdminPlatform/>} userType="admin" to="../admin/login"/>} >
+            <Route path="/admin/platform" element={<Private component={<AdminPlatform/>} userType="admin" />} >
                 <Route path="projects" element={<AdminProjects />} />
                 <Route path="stories" element={<h1>Stories</h1>} />
                 <Route path="personal-tasks" element={<h1>Tasks</h1>} />
                 <Route path="team-members" element={<h1>Members</h1>} />
             </Route>
 
-            <Route path="/team-member/platform" element={<Private component={<TeamMemberPlatform/>} userType="teamMember" to="../team-member/login"/>} >
+            <Route path="/team-member/platform" element={<Private component={<TeamMemberPlatform/>} userType="teamMember"/>} >
                 <Route path="projects" element={< AdminProjects />} />
                 <Route path="stories" element={<h1>Stories</h1>} />
                 <Route path="personal-tasks" element={<h1>Tasks</h1>} />
