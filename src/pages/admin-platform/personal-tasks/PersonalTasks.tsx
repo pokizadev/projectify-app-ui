b4720bced74e852ec7593d5d1d3c6399bff8b0da
PokizaSdv <pokizasdv@gmail.com>
2024-02-03 21:58:42 -0500

    Add Date Picker to Create Project Modal

--- src/pages/admin-platform/personal-tasks/PersonalTasks.tsx
+++ src/pages/admin-platform/personal-tasks/PersonalTasks.tsx
@@ -1,35 +1,42 @@
 import { useState } from "react";
 import styled from "styled-components";
-import {Input, Modal, Typography, Button} from "../../../design-system"
+import {
+    Input,
+    Modal,
+    Typography,
+    Button,
+    DatePickerV1
+} from "../../../design-system";
 import { NoDataPlaceholder } from "../../components";
-import noTask from "../../../assets/illustrations/no-tasks.svg"
+import noTask from "../../../assets/illustrations/no-tasks.svg";
 
 const PageBase = styled.div`
     position: relative;
     width: 100%;
     height: 100%;
-`
+`;
 
 const CreateTaskModalTitle = styled(Typography)`
     margin-bottom: var(--space-24);
-`
+`;
 
 const Inputs = styled.div`
     display: flex;
     flex-direction: column;
     gap: var(--space-16);
     margin-bottom: var(--space-24);
-`
+`;
 
 const Buttons = styled.div`
     display: flex;
     gap: var(--space-10);
-`
+`;
 
 const Tasks = () => {
-    const [tasks, setTasks] = useState<string[]>([])
-    const [showCreateTaskModal, setShowCreateTaskModal] = useState<boolean>(false)
-
+    const [tasks, setTasks] = useState<string[]>([]);
+    const [dueDate, setDueDate] = useState<Date>();
+    const [showCreateTaskModal, setShowCreateTaskModal] =
+        useState<boolean>(false);
 
     return (
         <PageBase>
@@ -39,7 +46,7 @@ const Tasks = () => {
                     text="You don't have any tasks yet!"
                     buttonText="Add a Task"
                     buttonAction={() => setShowCreateTaskModal(true)}
-                /> 
+                />
             ) : (
                 <h1>Tasks</h1>
             )}
@@ -49,11 +56,38 @@ const Tasks = () => {
                     New Task
                 </CreateTaskModalTitle>
                 <Inputs>
-                    <Input placeholder="Task Name" value="" onChange={() => {}} shape="rounded" size="lg"/>
-                    <Input type="textarea" placeholder="Task Description" value="" onChange={() => {}} shape="rounded" size="lg"/>
+                    <Input
+                        placeholder="Task Name"
+                        value=""
+                        onChange={() => {}}
+                        shape="rounded"
+                        size="lg"
+                    />
+                    <Input
+                        type="textarea"
+                        placeholder="Task Description"
+                        value=""
+                        onChange={() => {}}
+                        shape="rounded"
+                        size="lg"
+                    />
+                    <DatePickerV1
+                        inputSize="lg"
+                        shape="rounded"
+                        placeholder="Due Date"
+                        selected={dueDate}
+                        onChange={(date) => setDueDate(date)}
+                    />
                 </Inputs>
                 <Buttons>
-                    <Button color="secondary" size="lg" shape="rounded" variant="outlined" fullWidth onClick={() => setShowCreateTaskModal(false)}>
+                    <Button
+                        color="secondary"
+                        size="lg"
+                        shape="rounded"
+                        variant="outlined"
+                        fullWidth
+                        onClick={() => setShowCreateTaskModal(false)}
+                    >
                         Cancel
                     </Button>
                     <Button size="lg" shape="rounded" color="primary" fullWidth>
@@ -62,7 +96,7 @@ const Tasks = () => {
                 </Buttons>
             </Modal>
         </PageBase>
-    )
-}
+    );
+};
 
-export {Tasks as AdminTasks}
+export { Tasks as AdminPersonalTasks };
