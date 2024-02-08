8755094b4152234fcf925b12603b7fdb29299a5d
PokizaSdv <pokizasdv@gmail.com>
2024-02-08 14:13:25 -0500

    Refactor Admin Personal Tasks

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ export * from "./admin-auth/admin-login/AdminLogin"
 export * from "./admin-auth/admin-forgot-password/AdminForgotPassword"
 export * from "./admin-auth/admin-reset-password/AdminResetPassword"
 export * from "./admin-platform/AdminPlatform"
-export * from "./admin-platform/personal-tasks/PersonalTasks"
+export * from "./admin-platform/personal-tasks"
 export * from "./admin-platform/projects/Projects"
 export * from "./admin-platform/team-members/TeamMembers"
 
