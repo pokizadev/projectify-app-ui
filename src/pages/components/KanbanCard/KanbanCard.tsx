4a531b5a481a828f6bf73b31ac331e767f489c1b
PokizaSdv <pokizasdv@gmail.com>
2024-02-11 07:01:53 -0500

    Rename TaskCard to KanbanCard

--- src/pages/components/KanbanCard/KanbanCard.tsx
+++ src/pages/components/KanbanCard/KanbanCard.tsx
@@ -1,10 +1,10 @@
 import styled, { css } from "styled-components";
-import { TaskCardProps } from "./types";
+import { KanbanCardProps } from "./types";
 import { Badge, Bar, Menu, Typography } from "../../../design-system";
 import { format } from "date-fns";
 import { useState } from "react";
 
-const TaskCardBase = styled.div<{ $isDragging: boolean }>`
+const KanbanCardBase = styled.div<{ $isDragging: boolean }>`
     background-color: var(--white);
     padding: var(--space-16);
     border-radius: var(--border-radius-16);
@@ -25,7 +25,7 @@ const TaskCardBase = styled.div<{ $isDragging: boolean }>`
     }
 `;
 
-const TaskCardHeader = styled.div`
+const KanbanCardHeader = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -54,7 +54,7 @@ enum StatusToIcon {
     INPROGRESS = "flag",
     DONE = "check"
 }
-const TaskCard: React.FC<TaskCardProps> = ({
+const KanbanCard: React.FC<KanbanCardProps> = ({
     task,
     menuActions,
     onSelectMenuAction
@@ -74,16 +74,16 @@ const TaskCard: React.FC<TaskCardProps> = ({
         onSelectMenuAction(value, task.id);
     };
     return (
-        <TaskCardBase
+        <KanbanCardBase
             draggable
             onDragStart={onDragStart}
             onDragEnd={onDragEnd}
             $isDragging={isDragging}
         >
-            <TaskCardHeader>
+            <KanbanCardHeader>
                 <Bar color={StatusToColor[task.status]} />
                 <Menu items={menuActions} onSelect={handleOnSelectMenuItem} />
-            </TaskCardHeader>
+            </KanbanCardHeader>
             <div>
                 <TaskTitle variant="paragraphLG" weight="semibold">
                     {task?.title}
@@ -99,8 +99,8 @@ const TaskCard: React.FC<TaskCardProps> = ({
                 variant="contained"
                 shape="rounded"
             />
-        </TaskCardBase>
+        </KanbanCardBase>
     );
 };
 
-export { TaskCard };
+export { KanbanCard };
