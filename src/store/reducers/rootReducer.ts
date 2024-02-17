b18d6f900f3c0271f37cb8273ab28fdd386789de
PokizaSdv <pokizasdv@gmail.com>
2024-02-16 23:34:19 -0500

    Create and Fetch all Team Members

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -2,11 +2,13 @@ import { userReducer } from "./userReducer";
 import { adminTasksReducer } from "./adminTasksReducer";
 import { GlobalState } from "../state";
 import { ActionType } from "../actions";
+import { adminTeamMembersReducer } from "./adminTeamMemberReducer";
 
 const rootReducer = (state: GlobalState, action: ActionType): GlobalState => {
     const newState: GlobalState = {
         user: userReducer(state.user, action),
         adminPersonalTasks: adminTasksReducer(state.adminPersonalTasks, action),
+        teamMembers: adminTeamMembersReducer(state.teamMembers, action)
     };
 
     return newState;
--- src/store/state/state.ts
+++ src/store/state/state.ts
@@ -1,14 +1,17 @@
-import { Task, AdminUser, TeamMemberUser} from "../../types";
+import { Task, AdminUser, TeamMember, TeamMemberUser} from "../../types";
 
 export type UserState = AdminUser | TeamMemberUser | null;
 export type TaskState = Task[];
+export type TeamMemberState = TeamMember[]
 
 export interface GlobalState {
     user: UserState
     adminPersonalTasks: TaskState
+    teamMembers: TeamMemberState
 }
 
 export const initialState: GlobalState = {
     user: null,
-    adminPersonalTasks: []
+    adminPersonalTasks: [],
+    teamMembers: []
 };
