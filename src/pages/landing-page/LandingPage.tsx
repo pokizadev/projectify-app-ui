156db85b098146a417588a2abdf0998f1dc926e3
PokizaSdv <pokizasdv@gmail.com>
2024-04-27 09:46:13 -0400

    Create Features section

--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -1,14 +1,15 @@
 import { Companies } from "./sections/companies/Companies";
+import { Features } from "./sections/features/Features";
 import { HeroPage } from "./sections/hero/Hero";
 
 const LandingPage = () => {
     return (
         <>
-        <HeroPage />
-        <Companies/>
-
+            <HeroPage />
+            <Companies />
+            <Features />
         </>
-    )
+    );
 };
 
 export { LandingPage };
